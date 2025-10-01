8fb5fcf33095f0f106cd22c0de38d803d8a05d5d
alliballib <adjaynaebillings@gmail.com>
2025-09-30 23:44:01 -0500

    Add GA4 analytics integration

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,14 @@ import './index.css'
 import App from './App.jsx'
 import { ThemeProvider, CssBaseline, GlobalStyles } from '@mui/material'
 import theme from './theme.jsx'
+import ReactGA from 'react-ga4'
+
+const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+
+if (measurementId) {
+  ReactGA.initialize(measurementId)
+  ReactGA.send({ hitType: 'pageview', page: window.location.pathname + window.location.search })
+}
 
 createRoot(document.getElementById('root')).render(
   // Temporarily removed StrictMode to allow smooth loading animations
